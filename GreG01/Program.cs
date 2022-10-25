d5eb0b6054337f2558212ce4e340201d6f1be754
User <User@DESKTOP-ESP2JT1>
2022-10-25 15:14:17 +0300

    Add project files.

---
+++ GreG01/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // read figure
+        string figure = Console.ReadLine();
+        double area = 0;
+        if (figure == "square")
+        {
+            double side = double.Parse(Console.ReadLine());
+            area = side * side;
+        }
+        else if (figure == "rectangle")
+        {
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+
+            area = sideA * sideB;
+        }
+        else if (figure == "circle")
+        {
+            double radius = double.Parse(Console.ReadLine());
+            area = Math.PI * radius * radius;
+            Console.WriteLine(Math.PI);
+        }
+        else if (figure == "triangle")
+        {
+            double side = double.Parse((Console.ReadLine()));
+            double h = double.Parse((Console.ReadLine()));
+            area = h * side / 2;
+        }
+
+        {
+
+            Console.WriteLine($"{area:f2}");
+        }
+
+
+
+
+
+    }
+}
