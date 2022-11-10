a25904e51c151934853de0ee0fd7d80e98935f0c
Ts. Hristov <42769725+KiwiB0y@users.noreply.github.com>
2022-11-10 15:00:30 +0100

    Fix formatting
    Clear whitespaces and some other minor changes like remove the unnecessary curly braces.

--- GreG01/Program.cs
+++ GreG01/Program.cs
@@ -9,9 +9,11 @@ internal class Program
         // read figure
         string figure = Console.ReadLine();
         double area = 0;
+
         if (figure == "square")
         {
             double side = double.Parse(Console.ReadLine());
+            
             area = side * side;
         }
         else if (figure == "rectangle")
@@ -24,24 +26,17 @@ internal class Program
         else if (figure == "circle")
         {
             double radius = double.Parse(Console.ReadLine());
+            
             area = Math.PI * radius * radius;
-            Console.WriteLine(Math.PI);
         }
         else if (figure == "triangle")
         {
             double side = double.Parse((Console.ReadLine()));
             double h = double.Parse((Console.ReadLine()));
+            
             area = h * side / 2;
         }
 
-        {
-
-            Console.WriteLine($"{area:f2}");
-        }
-
-
-
-
-
+        Console.WriteLine($"{area:f2}");
     }
 }
